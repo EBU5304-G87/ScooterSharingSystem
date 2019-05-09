67fced3eb8cef54b41c90cfff9eb93c25ce6d294
GermanOrthopaedic <fanxianghe2017@gmail.com>
2019-05-09 22:24:46 +0800

    ID Check bug fixed

--- src/main/java/idCheck.java
+++ src/main/java/idCheck.java
@@ -1,41 +1,26 @@
-import java.util.Date;
 import java.util.Scanner;
 
-public class idCheck {
+public class IdCheck {
     public static void main(String[] args){
-        Database db = Database.getInstance();
-        System.out.println("please enter your id: ");
-        Scanner input = new Scanner(System.in);
-        String id = input.next();
-        boolean isUser = false;
-        int i, num = 0;
+       Database db = Database.getInstance();
+       Scanner input = new Scanner(System.in);
+       String id = input.next();
+       boolean isUser = false;
+       int i;
 
-        for(i = 0; i < db.users.size(); i++){
-            User u = db.users.get(i);
-            if(u.getId() == Integer.parseInt(id)){
-                isUser = true;
-                num = i;
-            }
-        }
+       for(i = 0; i < db.users.size(); i++){
+           User u = db.users.get(i);
+           if(u.getId() == Integer.parseInt(id)){
+               isUser = true;
+           }
+       }
 
-        if(isUser == true && db.users.get(num).isBorrowed() == false && db.users.get(num).isViolation() == false){
-            db.users.get(num).setBorrowed(true);
-            System.out.println("here");
-            Station a=new Station();
-            int isScooterAway=a.unlockSlot(0);
-            if(isScooterAway==1)
-            {
-                System.out.println("here");
-                db.records.add(new Record(db.users.get(num).getId(),new Date(0), new Date(0)));
+       if(isUser == true && db.users.get(i).isBorrowed() == false && db.users.get(i).isViolation() == false){
+           db.users.get(i).setBorrowed(true);
+           db.records.add(new Record(db.users.get(i).getId()));
+           db.records.get(db.records.size()-1).startRecord();
+       }
 
-                db.records.get(db.records.size() - 1).startRecord();
-            }
-            else
-            {
-                isUser=false;
-            }
-        }
-
-        db.writeUsers();
+       db.writeUsers();
     }
 }
